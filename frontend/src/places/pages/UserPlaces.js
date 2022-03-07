9e93a58b4117df10b6aa33a549de01e9ac58e2af
James McCarron <mccarron.j@gmail.com>
2022-03-07 13:02:08 +1100

    Added dummy place data to UserPlaces & new route in App.js

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,7 +1,38 @@
 import React from "react";
 
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Sydney Opera House",
+    description: "One of the most famous opera house in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Sydney_Opera_House%2C_vivid_Sydey.JPG/2880px-Sydney_Opera_House%2C_vivid_Sydey.JPG",
+    address: "Bennelong Point, Sydney NSW 2000",
+    location: {
+      lat: -33.8567844,
+      lng: 151.2152967,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Sydney Opera House",
+    description: "One of the most famous opera house in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Sydney_Opera_House%2C_vivid_Sydey.JPG/2880px-Sydney_Opera_House%2C_vivid_Sydey.JPG",
+    address: "Bennelong Point, Sydney NSW 2000",
+    location: {
+      lat: -33.8567844,
+      lng: 151.2152967,
+    },
+    creator: "u2",
+  },
+];
+
 const UserPlaces = () => {
-  return <div>UserPlaces</div>;
+  return <PlaceList items={DUMMY_PLACES} />;
 };
 
 export default UserPlaces;
