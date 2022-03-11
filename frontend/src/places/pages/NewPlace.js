b63d040b991360a50347c299dbf24870b2e1b3e8
James McCarron <mccarron.j@gmail.com>
2022-03-11 11:03:51 +1100

    Fixed useEffect dependencies in Input.js

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -64,6 +64,7 @@ const NewPlace = () => {
   return (
     <form className="place-form" onSubmit={placeSubmitHandler}>
       <Input
+        id="title"
         element="input"
         type="text"
         label="Title"
@@ -77,6 +78,14 @@ const NewPlace = () => {
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (at least 5 characters)."
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        element="input"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address."
         onInput={inputHandler}
       />
       <Button type="submit" disabled={!formState.isValid}>
