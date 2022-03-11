20e79accfa2c109ea09b4776ccbd864c35e5842e
James McCarron <mccarron.j@gmail.com>
2022-03-11 13:09:24 +1100

    Started on a new form in the UpdatePlace component

--- frontend/src/places/pages/UpdatePlace.js
+++ frontend/src/places/pages/UpdatePlace.js
@@ -1,7 +1,83 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Sydney Opera House",
+    description: "One of the most famous opera house in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Sydney_Opera_House%2C_vivid_Sydey.JPG/2880px-Sydney_Opera_House%2C_vivid_Sydey.JPG",
+    address: "Bennelong Point, Sydney NSW 2000",
+    location: {
+      lat: -33.8567844,
+      lng: 151.2152967,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Sydney Opera House",
+    description: "One of the most famous opera houses in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Sydney_Opera_House%2C_vivid_Sydey.JPG/2880px-Sydney_Opera_House%2C_vivid_Sydey.JPG",
+    address: "Bennelong Point, Sydney NSW 2000",
+    location: {
+      lat: -33.8567844,
+      lng: 151.2152967,
+    },
+    creator: "u2",
+  },
+];
 
 const UpdatePlace = () => {
-  return <h2>UpdatePlace</h2>;
+  const placeId = useParams().placeId;
+
+  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  if (!identifiedPlace) {
+    return (
+      <div className="center">
+        <h2>Could not find place!</h2>
+      </div>
+    );
+  }
+
+  return (
+    <form>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={() => {}}
+        value={identifiedPlace.title}
+        valid={true}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min. 5 characters)."
+        onInput={() => {}}
+        value={identifiedPlace.description}
+        valid={true}
+      />
+      <Button type="submit" disabled={true}>
+        UPDATE PLACE
+      </Button>
+    </form>
+  );
 };
 
 export default UpdatePlace;
