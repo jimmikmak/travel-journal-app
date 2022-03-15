b55a575717498045fa3f3e42518df5d2083e83c6
James McCarron <mccarron.j@gmail.com>
2022-03-15 17:37:35 +1100

    Added a specific route for p1 with some dummy data

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -3,10 +3,27 @@ const express = require("express");
 
 const router = express.Router();
 
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Sydney Opera House",
+    description: "One of the most famous opera houses in the world!",
+    location: {
+      lat: -33.8567844,
+      lng: 151.2152967,
+    },
+    address: "Bennelong Point, Sydney NSW 2000",
+    creator: "u1",
+  },
+];
+
 // eslint-disable-next-line no-unused-vars
-router.get("/", (req, res, next) => {
-  console.log("GET Request in Places");
-  res.json({ message: "It works!" });
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid; // { pid: "p1" }
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  res.json({ place }); // => { place } => { place: place }
 });
 
 module.exports = router;
