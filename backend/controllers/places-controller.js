573230a97e8b584df4180134b1e7249ae7bda8da
James McCarron <mccarron.j@gmail.com>
2022-03-18 14:15:51 +1100

    Syntax correction for error text in getPlacesByUserId

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -38,7 +38,7 @@ const getPlacesByUserId = (req, res, next) => {
 
   if (!places || places.length === 0) {
     return next(
-      new HttpError("Could not find a places for the provided user id.", 404)
+      new HttpError("Could not find places for the provided user id.", 404)
     );
   }
   res.json({ places });
