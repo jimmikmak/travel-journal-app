43a231c04e1cab2c1cd49bb9b705e2a26a8df461
James McCarron <mccarron.j@gmail.com>
2022-03-21 11:59:50 +1100

    Set-up new user routes

---
+++ backend/routes/users-routes.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const express = require("express");
+
+const usersController = require("../controllers/users-controller");
+
+const router = express.Router();
+
+router.get("/", usersController.getUsers);
+
+router.post("/signup", usersController.signup);
+
+router.post("/login", usersController.login);
+
+module.exports = router;
