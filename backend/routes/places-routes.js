6e9f2a73888b8dff28b9b478d8dc8c8e780bea12
James McCarron <mccarron.j@gmail.com>
2022-03-23 10:19:49 +1100

    Fixed the post route 'address' call

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -16,7 +16,7 @@ router.post(
   [
     check("title").not().isEmpty(),
     check("description").isLength({ min: 5 }),
-    check("address").not().isEmpty,
+    check("address").not().isEmpty(),
   ],
   placesControllers.createPlace
 );
