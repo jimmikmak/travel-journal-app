d67a4444db3dbdd0799ff74c779fde6bb06055fc
James McCarron <mccarron.j@gmail.com>
2022-03-25 12:56:50 +1100

    New util folder & location file with API key

---
+++ backend/util/location.js
@@ -0,0 +1,8 @@
+const API_KEY = "AIzaSyDRCHqnthnfmSLyjwVAbwICCZZBktvYtMg";
+
+const getCoordsForAddress = async (address) => {
+  // return {
+  //   lat: -33.8567844,
+  //   lng: 151.2152967,
+  // };
+};
