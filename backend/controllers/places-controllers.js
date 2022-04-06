b3a37cdc92272766081fbdeb8289438960d9d018
James McCarron <mccarron.j@gmail.com>
2022-04-06 13:52:07 +1000

    Removed the dummy code from deletePlace in places-controllers

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -148,10 +148,7 @@ const updatePlace = async (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
-  if (!DUMMY_PLACES.find((p) => p.id === placeId)) {
-    throw new HttpError("Could not find a place with that id.", 404);
-  }
-  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+
   res.status(200).json({ message: "Deleted place." });
 };
 
