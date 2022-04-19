5e28fc2022dcb5c8affebe53494a09235f55468e
James McCarron <mccarron.j@gmail.com>
2022-04-19 12:54:38 +1000

    Forgot to import mongoose in places-controllers

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 const { v4: uuid } = require("uuid");
 const { validationResult } = require("express-validator");
+const mongoose = require("mongoose");
 
 const HttpError = require("../models/http-error");
 const getCoordsForAddress = require("../util/location");
