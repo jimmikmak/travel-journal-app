799c1eac4155d19d13b1dd8bb2a895a193c20785
James McCarron <mccarron.j@gmail.com>
2022-04-22 13:09:39 +1000

    Added new loading & error states to Auth.js

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -15,6 +15,9 @@ import "./Auth.css";
 const Auth = () => {
   const auth = useContext(AuthContext);
   const [isLoginMode, setIsLoginMode] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
   const [formState, inputHandler, setFormData] = useForm(
     {
       email: {
@@ -59,6 +62,7 @@ const Auth = () => {
     if (isLoginMode) {
     } else {
       try {
+        setIsLoading(true);
         const response = await fetch("http://localhost:5555/api/users/signup", {
           method: "POST",
           headers: {
@@ -73,12 +77,14 @@ const Auth = () => {
 
         const responseData = await response.json();
         console.log(responseData);
+        setIsLoading(false);
+        auth.login();
       } catch (err) {
         console.log(err);
+        setIsLoading(false);
+        setError(err.message || "Something went wrong, please try again.");
       }
     }
-
-    auth.login();
   };
 
   return (
