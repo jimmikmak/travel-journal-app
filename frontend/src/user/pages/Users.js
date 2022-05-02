6557d60c11b348f48d3bf72aec254900559a9fd8
James McCarron <mccarron.j@gmail.com>
2022-05-02 14:30:17 +1000

    Removed the dummy users

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -3,15 +3,8 @@ import React from "react";
 import UsersList from "../components/UsersList";
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "JimmyMac",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Sydney_Opera_House%2C_vivid_Sydey.JPG/2880px-Sydney_Opera_House%2C_vivid_Sydey.JPG",
-      places: 3,
-    },
-  ];
+  const USERS = 
+
 
   return <UsersList items={USERS} />;
 };
