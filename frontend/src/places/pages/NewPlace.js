0af6f4494735c309f0804bfc610ad9140021f7ec
James McCarron <mccarron.j@gmail.com>
2022-05-04 13:50:11 +1000

    imported useHistory

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
