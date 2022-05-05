eb08ec7bd0e9560f5643dffe87f8f915f452dbbe
James McCarron <mccarron.j@gmail.com>
2022-05-05 16:39:14 +1000

    Started modifying UserPlaces function

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,40 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import PlaceList from "../components/PlaceList";
-
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Sydney Opera House",
-    description: "One of the most famous opera house in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Sydney_Opera_House%2C_vivid_Sydey.JPG/2880px-Sydney_Opera_House%2C_vivid_Sydey.JPG",
-    address: "Bennelong Point, Sydney NSW 2000",
-    location: {
-      lat: -33.8567844,
-      lng: 151.2152967,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Syd. Opera House",
-    description: "One of the most famous opera houses in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Sydney_Opera_House%2C_vivid_Sydey.JPG/2880px-Sydney_Opera_House%2C_vivid_Sydey.JPG",
-    address: "Bennelong Point, Sydney NSW 2000",
-    location: {
-      lat: -33.8567844,
-      lng: 151.2152967,
-    },
-    creator: "u2",
-  },
-];
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const UserPlaces = () => {
+  const [loadedPlaces, setLoadedPlaces] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      const response = await sendRequest();
+    };
+    fetchPlaces();
+  }, [sendRequest]);
+
   return <PlaceList items={loadedPlaces} />;
 };
 
