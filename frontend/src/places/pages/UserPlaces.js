4c7d85e566cc1fa00d41ce0ad1c8258c3954328f
James McCarron <mccarron.j@gmail.com>
2022-05-06 11:14:17 +1000

    Another 'failed to create place' error

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import PlaceList from "../components/PlaceList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const UserPlaces = () => {
@@ -12,12 +14,27 @@ const UserPlaces = () => {
 
   useEffect(() => {
     const fetchPlaces = async () => {
-      const response = await sendRequest();
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5555/api/places/user/${userId}`
+        );
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
     };
     fetchPlaces();
-  }, [sendRequest]);
+  }, [sendRequest, userId]);
 
-  return <PlaceList items={loadedPlaces} />;
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </React.Fragment>
+  );
 };
 
 export default UserPlaces;
