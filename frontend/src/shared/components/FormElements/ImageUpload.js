4d67a837b2b9e52c88c06b52807a077f2c5aa98e
James McCarron <mccarron.j@gmail.com>
2022-05-13 13:29:56 +1000

    Started to work on the input render in ImageUpload

--- frontend/src/shared/components/FormElements/ImageUpload.js
+++ frontend/src/shared/components/FormElements/ImageUpload.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 
+import Button from "./Button";
 import "./ImageUpload.css";
 
-const ImageUpload = (props) => {};
+const ImageUpload = (props) => {
+  const filePickerRef = useRef();
+
+  const pickImageHandler = () => {
+    filePickerRef.current.click();
+  };
+
+  return (
+    <div className="form-control">
+      <input
+        id={props.id}
+        ref={filePickerRef}
+        style={{ displaly: "none" }}
+        type="file"
+        accept=".jpg,.png,.jpeg"
+      />
+      <div className={`image-upload ${props.center && "center"}`}>
+        <div className="image-upload__preview">
+          <img src="" alt="Preview" />
+        </div>
+        <Button type="button" onClick={pickImageHandler}></Button>
+      </div>
+    </div>
+  );
+};
 
 export default ImageUpload;
