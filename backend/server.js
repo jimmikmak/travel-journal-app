def9ee45c6ca253a0102fdc907b13d831df05789
James McCarron <mccarron.j@gmail.com>
2022-05-17 12:14:54 +1000

    404 not found - failed to display image

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,11 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
+    });
+  }
   if (res.headerSent) {
     return next(error);
   }
