49b7944ba688c4edd09ff04cbbfba09dd70251d4
James McCarron <mccarron.j@gmail.com>
2022-05-19 13:43:52 +1000

    Created new check-auth file

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -12,6 +12,8 @@ router.get("/:pid", placesControllers.getPlaceById);
 
 router.get("/user/:uid", placesControllers.getPlacesByUserId);
 
+router.use();
+
 router.post(
   "/",
   fileUpload.single("image"),
